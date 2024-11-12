c042250792f0f30f4001e95c1cbdd98b576623b5
lee88688@163.com <lee>
2024-11-12 18:41:50 +0800

    fix: type error

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.tsx';
+import App from './App.tsx';
 import './index.css';
 import './vscode.ts';
 import { showErrorMessage, webviewReady } from './commandApi.ts';
